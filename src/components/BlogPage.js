6bf0b4238f3fc9d4bdf60f78fb9a0dc5640d1d34
PJ Jones <pj.jones4000.com>
2022-05-24 12:26:27 -0500

    done w main page for now, working on blog page

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
+import img from '../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg'
+
+
+const MainContainer = styled.div`
+background-image: url(${img});
+background-size: cover;
+background-repeat: no-repeat;
+background-attachment: fixed;
+background-position: center;
+width: 100vw;
+height: 100vh;
+`
+
 
 const BlogPage = () => {
   return (
-    <div>
-      BlogPage Component/Page
-    </div>
+    <MainContainer>
+      
+    </MainContainer>
   )
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -147,30 +147,45 @@ const Main = () => {
         </Center>
 
         <Contact target="_blank" to={{pathname:"mailto:pj.jones4000@gmail.com"}}>
-          <h2>
+          <motion.h2
+          whileHover={{scale: 1.1}}
+          whileTap={{scale: 0.9}}
+          >
             Say hi..
-          </h2>
+          </motion.h2>
         </Contact>
         <BLOG to='/blog'>
-          <h2>
+          <motion.h2
+          whileHover={{scale: 1.1}}
+          whileTap={{scale: 0.9}}
+          >
             Blog
-          </h2>
+          </motion.h2>
         </BLOG>
         <WORK to='/work' click={click}>
-          <h2>
+          <motion.h2
+          whileHover={{scale: 1.1}}
+          whileTap={{scale: 0.9}}
+          >
             Work
-          </h2>
+          </motion.h2>
         </WORK>
         <BottomBar>
         <ABOUT to='/about' click={click}>
-          <h2>
+          <motion.h2
+          whileHover={{scale: 1.1}}
+          whileTap={{scale: 0.9}}
+          >
             About
-          </h2>
+          </motion.h2>
         </ABOUT>
         <SKILLS to='/skills'>
-          <h2>
+          <motion.h2
+          whileHover={{scale: 1.1}}
+          whileTap={{scale: 0.9}}
+          >
             My Skills
-          </h2>
+          </motion.h2>
         </SKILLS>
         </BottomBar>
       </Container>
