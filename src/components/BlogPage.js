77466b04cef955f8f816aaca0d72fc875fecdb4b
PJ Jones <pj.jones4000.com>
2022-05-24 14:25:26 -0500

    added a rough vector portrait of myself

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 import img from '../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg'
+import PowerButton from '../subComponents/PowerButton'
+import LogoComponent from '../subComponents/LogoComponent'
+import SocialIcons from '../subComponents/SocialIcons'
 
 
 const MainContainer = styled.div`
@@ -13,11 +16,47 @@ width: 100vw;
 height: 100vh;
 `
 
+const Container = styled.div`
+background-color: ${props => `rgba(${props.theme.bodyRgba},0.8)`};
+width: 100%;
+height: 100vh;
+position: relative;
+padding-bottom: 5rem;
+`
+
+const Center = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+padding-top: 10rem;
+`
+
+const Grid = styled.div`
+display: grid;
+grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
+grid-gap: calc(1rem + 2vw);
+`
 
 const BlogPage = () => {
   return (
     <MainContainer>
-      
+      <Container>
+        <LogoComponent />
+        <PowerButton />
+        <SocialIcons />
+        <Center>
+          <Grid>
+            <h1>Blog</h1>
+            <h1>Blog</h1>
+            <h1>Blog</h1>
+            <h1>Blog</h1>
+            <h1>Blog</h1>
+            <h1>Blog</h1>
+            <h1>Blog</h1>
+          </Grid>
+
+        </Center>
+      </Container>
     </MainContainer>
   )
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import {motion} from 'framer-motion'
-import Me from "../assets/Images/profile-img.png";
+import Me from "../assets/Images/roughportrait.png";
 
 const Box = styled(motion.div)`
   position: absolute;
@@ -41,7 +41,7 @@ const SubBox = styled.div`
 
   .pic {
     position: absolute;
-    bottom: 0;
+    bottom: -20%;
     left: 50%;
     transform: translate(-50%, 0%);
     width: 100%;
